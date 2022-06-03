c42f1ff5dfffadf79e4f8ca097721c6f1e870dfa
bcarlton <brittanyrc95@gmail.com>
2022-06-03 13:09:55 -0400

    Create function to get password length criteria from user

--- script.js
+++ script.js
@@ -3,12 +3,19 @@
 // prompt user for password length (between 8 and 128), validate and return value
 function getPasswordLength() {
     // variable for password length input
+    var passwordLength;
 
     // request valid input while passwordLength is outside 8-128 range or NaN
-        // use Number(passwordLength) to change string input to number
+    passwordLength = prompt("Please enter a whole number for the length of your password between 8 and 128.")
+    
+    // validate user input
+    while (passwordLength < 8 || passwordLength > 128 || Number.isNaN(Number(passwordLength))) {
+        // console.log(Number(passwordLength));
+        passwordLength = prompt("Make sure to enter a whole number (no decimals) between 8 and 128.");
+    }
 
-    // return valid length choice
-    return;
+    // use Number(passwordLength) to change string input to number and return
+    return Number(passwordLength);
 }
 
 // prompt user to confirm use of character types and return object with charTypeName: boolean attributes
@@ -30,10 +37,14 @@ function getCharTypes() {
 // TODO: create generatePassword function
 // generate password based on user choices
 function generatePassword() {
+    // prompt user for criteria
     // get password length from user
-
+    var passwordLength = getPasswordLength();
     // get character types to be used from user
+    var charTypes = getCharTypes();
 
+    // debugger;
+    console.log("Password length:", passwordLength, "Character types:", charTypes);
     // create object with arrays of characters for each chosen char type
 
     // generate password using given criteria

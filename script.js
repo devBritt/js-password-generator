c23c0fd04f76227f96e797fa5d883c19b7f75533
bcarlton <brittanyrc95@gmail.com>
2022-06-03 12:34:57 -0400

    Create function skeletons, pseudocode

--- script.js
+++ script.js
@@ -1,5 +1,46 @@
 // Assignment code here
+// TODO: create criteriaPrompts function
+// prompt user for password length (between 8 and 128), validate and return value
+function getPasswordLength() {
+    // variable for password length input
 
+    // request valid input while passwordLength is outside 8-128 range or NaN
+        // use Number(passwordLength) to change string input to number
+
+    // return valid length choice
+    return;
+}
+
+// prompt user to confirm use of character types and return object with charTypeName: boolean attributes
+function getCharTypes() {
+    // charTypes object
+
+    // confirm lowercase, add choice to object
+
+    // confirm uppercase, add choice to object
+
+    // confirm numeric, add choice to object
+
+    // confirm special, add choice to object
+
+    // return charTypes
+    return;
+}
+
+// TODO: create generatePassword function
+// generate password based on user choices
+function generatePassword() {
+    // get password length from user
+
+    // get character types to be used from user
+
+    // create object with arrays of characters for each chosen char type
+
+    // generate password using given criteria
+    
+    // return password
+    return;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
